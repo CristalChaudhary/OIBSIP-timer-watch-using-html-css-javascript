3a8615fbf03a9b4306c2bc46af3ce981d6c4fe6d
cristal chaudhary <129609932+CristalChaudhary@users.noreply.github.com>
2023-06-11 11:46:00 +0530

    commit

---
+++ connect.js
@@ -0,0 +1,24 @@
+const endDate =" 16 June 2023 10:00 PM";
+document.getElementById("endDate").innerHTML=endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock(){
+    const end =new Date(endDate);
+    const now =new Date();
+    const diff = (end - now)/1000;
+    
+    inputs[0].value = Math.floor(diff/3600/24);
+    inputs[1].value = Math.floor(diff/3600)% 24;
+    inputs[2].value = Math.floor(diff/60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+clock()
+
+setInterval(
+     () => {
+
+        clock()
+    },
+    1000
+)
